a78797c78ec7c31c0fb4b54333a127426fdf7027
Jordan Wambaugh <jordan@wambaugh.org>
2023-10-30 14:36:34 -0700

    pretty print JSON

--- server/utils/schemaUpdate.ts
+++ server/utils/schemaUpdate.ts
@@ -1,28 +1,27 @@
+import * as fs from "fs";
 
-import * as fs from 'fs'
-
-const getSchemaPath = (model: string): string => {  
-  return `./src/api/${model}/content-types/${model}/schema.json`
-}
+const getSchemaPath = (model: string): string => {
+  return `./src/api/${model}/content-types/${model}/schema.json`;
+};
 
 const getSchema = (model: string) => {
-  return JSON.parse(fs.readFileSync(getSchemaPath(model)).toString())
-} 
+  return JSON.parse(fs.readFileSync(getSchemaPath(model)).toString());
+};
 
 const updateSchema = (model: string, data: Object) => {
-  fs.writeFileSync(getSchemaPath(model), JSON.stringify(data))
-}
+  fs.writeFileSync(getSchemaPath(model), JSON.stringify(data, null, 2));
+};
 
 export default () => ({
-  addAttribute(model: string, field: string, config: {[key: string]: any}) {    
-    let schema = getSchema(model)
-    schema.attributes[field] = config
+  addAttribute(model: string, field: string, config: { [key: string]: any }) {
+    let schema = getSchema(model);
+    schema.attributes[field] = config;
 
-    updateSchema(model, schema)
+    updateSchema(model, schema);
   },
   removeAttribute(model: string, attribute_key: string) {
-    let schema = getSchema(model)
-    delete schema.attributes[attribute_key]
-    updateSchema(model, schema)
-  }
-})
+    let schema = getSchema(model);
+    delete schema.attributes[attribute_key];
+    updateSchema(model, schema);
+  },
+});
